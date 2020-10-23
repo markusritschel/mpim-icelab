39c7263acdd2b116b938dd9b2b76baaa488a1777
markusritschel <git@markusritschel.de>
2020-10-23 14:29:40 +0200

    Add tests for salinity harps

---
+++ tests/test_harps/test_salinity_harps.py
@@ -0,0 +1,70 @@
+# !/usr/bin/env python
+# -*- coding utf-8 -*-
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  kontakt@markusritschel.de
+# Date:   11/10/2020
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import numpy
+import pytest
+import xarray
+from mpim_icelab.harps import read_salinity_harp
+from mpim_icelab.harps.salinity_harps import calc_brine_salinity, calc_freezing_starts
+
+
+TESTFILE = './test_harps/salinity_harps_test_data.dat'
+
+@pytest.fixture
+def harp_dataset(file=TESTFILE):
+    return read_salinity_harp(file)
+
+
+def test_salinity(harp_dataset):
+    assert isinstance(harp_dataset, xarray.Dataset)
+    assert harp_dataset.coords['time'].dtype == 'datetime64[ns]'
+
+
+@pytest.mark.parametrize("method", ['Assur', 'N&W09', 'Vancoppenolle'])
+def test_calc_brine_salinity(harp_dataset, method):
+    T = numpy.random.random(10)
+    S_brine = calc_brine_salinity(T, method=method)
+    assert isinstance(S_brine, numpy.ndarray)
+    assert len(T) == len(S_brine)
+
+
+@pytest.mark.parametrize("method", ['median', 'savgol'])
+def test_calc_freezing_starts(harp_dataset, method):
+    freezing_starts = calc_freezing_starts(harp_dataset, 'r16', kind=method, tolerance=1e-4)
+    assert isinstance(freezing_starts, xarray.DataArray)
+    assert freezing_starts.size == harp_dataset.wire_pair.size * harp_dataset.module.size
+    assert freezing_starts.shape == (harp_dataset.module.size, harp_dataset.wire_pair.size)
+
+
+def test_calc_ice_properties(harp_dataset):
+    data = harp_dataset.copy().seaice.calc_ice_properties()
+    assert isinstance(data, xarray.Dataset)
+    for var in ['temperature', 'brine_salinity', 'liquid_fraction', 'solid_fraction']:
+        assert var in data.data_vars, f"{var} not found in dataset after calculating ice properties"
+        assert hasattr(data[var], 'units')
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_computed_quantities(harp_dataset):
+    for var in ['brine salinity', 'solid fraction', 'liquid fraction', 'bulk salinity']:
+        assert var in harp_dataset.data_vars
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_reference_resistance(harp_dataset):
+    resistance = harp_dataset['r16']
+    freezing_onset = harp_dataset['freezing_start']
+    assert isinstance(resistance, xarray.DataArray)
+    assert resistance['wire_harps'].size == freezing_onset
+
+
+# @pytest.mark.skip(reason="no way of currently testing this")
+def test_solid_liquid_fraction(harp_dataset):
+    assert numpy.all((harp_dataset.liquid_fraction + harp_dataset.solid_fraction).dropna('time') == 1), \
+        'Liquid and solid fraction should match up to 1.'
