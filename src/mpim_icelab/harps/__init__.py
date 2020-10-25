d2f6fb01d3f6aff32df9da889716d9a4fe286b22
markusritschel <git@markusritschel.de>
2020-10-25 22:30:34 +0100

    Rename salinity_harps.read_harp routine

--- src/mpim_icelab/harps/__init__.py
+++ src/mpim_icelab/harps/__init__.py
@@ -8,8 +8,7 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 from mpim_icelab.harps import salinity_harps
-from mpim_icelab.harps.salinity_harps import read_harp
+from mpim_icelab.harps.salinity_harps import read_salinity_harp
 from mpim_icelab.harps.helpers import *
 
-read_salinity_harp = read_harp
 # calc_brine_salinity = salinity_harps.calc_brine_salinity
